76a9f20b3dfa4cac696102e4360b2ea316f7b31b
katmaticdev2 <katmaticdev2@gmail.com>
2023-09-05 16:58:26 +0545

    Implemented Update Category

--- YourBookWeb/Controllers/CategoryController.cs
+++ YourBookWeb/Controllers/CategoryController.cs
@@ -33,6 +33,32 @@ namespace YourBookWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
+        } //Get
+        public IActionResult Edit(int? id)
+        {
+            if(id== null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
