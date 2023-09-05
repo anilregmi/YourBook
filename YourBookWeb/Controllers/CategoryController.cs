94e0fb03a0a97ed24b10ba1b7de297a03c1735b1
katmaticdev2 <katmaticdev2@gmail.com>
2023-09-05 14:37:55 +0545

    Implementation of Category

---
+++ YourBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourBookWeb.Data;
+using YourBookWeb.Models;
+
+namespace YourBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db= db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList=_db.categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ YourBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YourBookWeb.Models;
+
+namespace YourBookWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category>categories { get; set; }
+    }
+}
